0d80b43c74df1210910b59895c4608287dfd4f27
Usman Khalid <2200617@gmail.com>
2015-06-10 14:32:01 +0500

    Fixed import issue that was causing test failures.

--- openedx/core/djangoapps/bookmarks/signals.py
+++ openedx/core/djangoapps/bookmarks/signals.py
@@ -4,7 +4,6 @@ Signals for bookmarks.
 from django.dispatch.dispatcher import receiver
 
 from xmodule.modulestore.django import SignalHandler
-from .tasks import update_xblocks_cache
 
 
 @receiver(SignalHandler.course_published)
@@ -12,7 +11,8 @@ def trigger_update_xblocks_cache_task(sender, course_key, **kwargs):  # pylint: 
     """
     Trigger update_xblocks_cache() when course_published signal is fired.
     """
+    from .. import bookmarks  # Importing tasks too early causes issues in tests.
 
     # Note: The countdown=0 kwarg is set to ensure the method below does not attempt to access the course
     # before the signal emitter has finished all operations. This is also necessary to ensure all tests pass.
-    update_xblocks_cache.apply_async([unicode(course_key)], countdown=0)
+    bookmarks.tasks.update_xblocks_cache.apply_async([unicode(course_key)], countdown=0)
